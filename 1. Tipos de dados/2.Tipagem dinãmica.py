f625cd57e73189d9892479c835481a1d122228a5
T14G0S04R35 <161044147+T14G0S04R35@users.noreply.github.com>
2024-05-03 12:24:14 +0000

    Primeiros passos

---
+++ 1. Tipos de dados/2.Tipagem dinãmica.py
@@ -0,0 +1,18 @@
+# Tipagem dinãmica.
+
+# Atribuindo valores
+nome = "Marta"
+idade = 20
+peso = 62.000
+
+# Exidindo.
+# Simples.
+print(nome)
+print(idade)
+print(peso)
+
+# Formatado.
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso : {peso}")
+
---
+++ 1. Tipos de dados/3. Tipos estática.py
@@ -0,0 +1,11 @@
+# Tipagem dinãmica.
+
+# Atribuindo valores
+nome: str = "Marta"
+idade: int = 20
+peso: float = 62.000
+
+# Formatado.
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso : {peso}")
